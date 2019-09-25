c58260f782859ec30a17a3af28ef2a48715341c9
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-09-25 12:24:03 +0100

    Adds form to LandingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  const [name, setName] = React.useState("LaLeonie");
+  const updateName = event => {
+    setName(event.target.value);
+  };
+
+  return (
+    <main>
+      <h1>Welcome to our game!</h1>
+      <h2>Enter your Github name to get started</h2>
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={updateName}
+          placeholder="Your Name"
+        />
+        <p>{name}</p>
+        <button>Submit</button>
+      </form>
+    </main>
+  );
 };
 
 export default LandingPage;
