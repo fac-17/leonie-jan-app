e7f36f05a0136112ae9e53f4a0352df80011c54b
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-09-25 16:35:51 +0100

    Adds Followers.js to add followers at random position still static

--- src/components/PlayingField.js
+++ src/components/PlayingField.js
@@ -1,32 +1,39 @@
 import React from "react";
 import "./PlayingField.css";
 import Player from "./Player";
+import Follower from "./Follower";
 
-const PlayingField = ({githubObject,followersData}) => {
-  console.log(followersData)
-  const [followers,setFollowers]=React.useState([]);
-  const randomInt=(max)=>Math.floor(Math.random()*max)
-  React.useEffect(()=>{
-    setFollowers(followersData.map(follower=>{
-      return {
-        img:follower.avatar_url,
-        name:follower.login,
-        x:randomInt(800),
-        y:randomInt(800),
-        dx:randomInt(10)-5,
-        dy:randomInt(10)-5,
-        visible:true,
-        timer:0,
-      }
-    }));
-  },[followersData,githubObject])
-  return <div>
-    <Player  githubObject={githubObject} />
-    
-    {followers
-      .filter(follower=>follower.visible)
-      .map(follower=><div key={follower.name}>{follower.name},<img src={follower.img} alt="follower"/> {follower.x},{follower.y}</div>)}
-  </div>;
+const PlayingField = ({ githubObject, followersData }) => {
+  console.log(followersData);
+  const [followers, setFollowers] = React.useState([]);
+  const randomInt = max => Math.floor(Math.random() * max);
+  React.useEffect(() => {
+    setFollowers(
+      followersData.map(follower => {
+        return {
+          img: follower.avatar_url,
+          name: follower.login,
+          x: randomInt(800),
+          y: randomInt(800),
+          dx: randomInt(10) - 5,
+          dy: randomInt(10) - 5,
+          visible: true,
+          timer: 0
+        };
+      })
+    );
+  }, [followersData, githubObject]);
+  return (
+    <div>
+      <Player githubObject={githubObject} />
+
+      {followers
+        .filter(follower => follower.visible)
+        .map(follower => (
+          <Follower key={follower.name} follower={follower} />
+        ))}
+    </div>
+  );
 };
 
 export default PlayingField;
