2b837088197bc4f2bd11d9b44a5f255eb76d58a3
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-09-25 12:36:55 +0100

    Adds name and setname state

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,25 +1,40 @@
 import React from "react";
 
 const LandingPage = () => {
-  const [name, setName] = React.useState("LaLeonie");
-  const updateName = event => {
-    setName(event.target.value);
+  const [input, setInput] = React.useState("LaLeonie");
+  const [name, setName] = React.useState("");
+  const [githubObject, setGithubObject] = React.useState(null);
+
+  const updateInput = event => {
+    setInput(event.target.value);
   };
+
+  const updateName = event => {
+    event.preventDefault();
+    setName(input);
+  };
+
+  React.useEffect(() => {
+    console.log(name);
+  }, [name]);
 
   return (
     <main>
       <h1>Welcome to our game!</h1>
       <h2>Enter your Github name to get started</h2>
-      <form>
+      <form onSubmit={updateName}>
         <input
           type="text"
-          value={name}
-          onChange={updateName}
+          value={input}
+          onChange={updateInput}
           placeholder="Your Name"
         />
-        <p>{name}</p>
+        <p>
+          {input},{name}
+        </p>
         <button>Submit</button>
       </form>
+      <button>Start Playing</button>
     </main>
   );
 };
