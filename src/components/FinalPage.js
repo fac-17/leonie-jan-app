c1d98a82ac6d499c4938ee62bd71bef2c57806bc
Jan Skowronski <crianonim@gmail.com>
2019-09-27 12:05:38 +0100

    Makes hiscore key unique and show current score on the list

--- src/components/FinalPage.js
+++ src/components/FinalPage.js
@@ -1,12 +1,15 @@
 import "./FinalPage.css";
-import React from "react";
+import React, { useState } from "react";
 import HighScoreBoard from "./HighScoreBoard";
 
 const FinalPage = ({ score, killer, name, setPage, setScore }) => {
+  const [hs,setHs]=useState(null);
   React.useEffect(() => {
     let hs = JSON.parse(localStorage.getItem("hs") || "[]");
     hs.push({ name, killer, score });
     localStorage.setItem("hs", JSON.stringify(hs));
+    const sorted = hs.sort((a, b) => b.score - a.score).slice(0, 10);
+    setHs(sorted)
   }, [score, killer, name]);
   return (
     <main className="FinalPage">
@@ -23,7 +26,8 @@ const FinalPage = ({ score, killer, name, setPage, setScore }) => {
       >
         Play Again
       </button>
-      <HighScoreBoard />
+      {hs && <HighScoreBoard hiscore={hs}/>}
+      
     </main>
   );
 };
